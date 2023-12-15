ea07763d047f70ef92325fd811b5180bc4eaedaa
Timothee <tgayet@student.42lyon.fr>
2023-12-15 18:16:51 +0100

    Loic said that I should do it all over again

--- CPP00/ex01/Contact.hpp
+++ CPP00/ex01/Contact.hpp
@@ -1,38 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*   Contact.hpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tgayet <tgayet@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/11 19:07:17 by tgayet            #+#    #+#             */
-/*   Updated: 2023/12/15 18:14:49 by tgayet           ###   ########lyon.fr   */
+/*   Created: 2023/12/15 18:10:16 by tgayet            #+#    #+#             */
+/*   Updated: 2023/12/15 18:14:11 by tgayet           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
+#include <string>
+#include <algorithm>
 
-int	main(int argc, char **argv) {
+class Contact {
+	public:
 	
-	int	i = 0;
-	int	j = -1;
+		int			id;
+		std::string firstname;
+		std::string	lastname;
+		std::string	nickname;
+		std::string	phone_number;
+		std::string	darkest_secrets;
 
-	if (argc < 2) {
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
-		return 0;
-	}
-
-	while (argv[++i]) {
-		j = -1;
-
-		while (argv[i][++j]){
-			if (argv[i][j] > 96 && argv[i][j] < 123)
-				std::cout << (char)(argv[i][j] - 32);
-			else
-				std::cout << argv[i][j];
-		}
-	}
-	std::cout << '\n';
-
-	return 0;
-}
+		Contact(void);
+};
