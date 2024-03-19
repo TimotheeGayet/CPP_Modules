0f0840051347300bddb4c7b4191d8b7c000d54e0
TimotheeGayet <79696450+TimotheeGayet@users.noreply.github.com>
2024-03-19 13:05:30 +0000

    2nd correction CPP04

--- CPP04/ex01/Brain.cpp
+++ CPP04/ex01/Brain.cpp
@@ -7,15 +7,15 @@ Brain::Brain(void)
 
 Brain::Brain(const Brain &brain)
 {
-    *this = brain;
     std::cout << "Brain copy constructor called" << std::endl;
+    *this = brain;
 }
 
 Brain &Brain::operator=(const Brain &brain)
 {
+    std::cout << "Brain assignation operator called" << std::endl;
     for (int i = 0; i < 100; i++)
         this->ideas[i] = brain.ideas[i];
-    std::cout << "Brain assignation operator called" << std::endl;
     return *this;
 }
 
