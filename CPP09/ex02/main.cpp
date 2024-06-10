2019a96b6be051bf93db8a40cd96bc382629fb3b
TimLeHamster <79696450+TimLeHamster@users.noreply.github.com>
2024-06-10 19:36:02 +0200

    nearly good

--- CPP09/ex02/main.cpp
+++ CPP09/ex02/main.cpp
@@ -2,7 +2,13 @@
 #include <algorithm>
 #include <iostream>
 
-int main(int argc, char** argv) {
-    Pmergeme(argc, argv);
-    return 0;
+int main(int argc, char **argv)
+{
+    if (argc != 2)
+    {
+        std::cerr << "Usage: ./PmergeMe 1 2 3 4 5 6 7 8 9 10" << std::endl;
+        return 1;
+    }
+    argv += 1;
+    std::vector<int> container = PmergeMe::convertToContainer(argv);
 }
